24d8c0eb79abf2570c9b5c602e246d17c5327728
Ashin Varghese <ashinv101@gmail.com>
2025-07-24 23:58:45 +0530

    ltstss

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -73,25 +73,40 @@ return (
         </div>
 
         {/* Project 3 */}
-{/* Project 3 */}
-<div className="project__card">
- <i className="uil uil-capsule services__icon"></i>
- <h3 className="project__title">MediKart</h3>
- <p className="project__desc">
-  <br/>
-   I am developing a full-stack online medical store that reads uploaded prescriptions and ensures safe delivery of medicines to customers.
- </p>
- <ul className="project__list">
-      
-   <li>Built using ReactJS, NodeJS, ExpressJS</li>
-   <br/>
-   <li>Database: MongoDB and Firebase</li>
- </ul>
-</div>
+        <div className="project__card">
+        <i className="uil uil-capsule services__icon"></i>
+        <h3 className="project__title">MediKart</h3>
+        <p className="project__desc">
+          <br/>
+          I am developing a full-stack online medical store that reads uploaded prescriptions and ensures safe delivery of medicines to customers.
+        </p>
+        <ul className="project__list">
+              
+          <li>Built using ReactJS, NodeJS, ExpressJS</li>
+          <br/>
+          <li>Database: MongoDB and Firebase</li>
+        </ul>
+        </div>
 
       </div>
     </section>
   </div>
+
+<br/>
+  <div className="about__box">
+    <h3 className="about__title">Internships</h3>
+    <div className="education__container">
+
+      <div className="education__card">
+        <i className="uil uil-briefcase-alt education__icon"></i>
+        <h4 className="education__degree">CIAL</h4>
+        <p className="education__institute">Completed a one-month project in the IT Department at<br></br> Cochin International Airport.</p>
+        <p className="education__details">May 2025 - June 2025</p>
+      </div>
+
+    </div>
+  </div>
+
 </div>
 );
 };
